a9b81d50cb00c2fb672257796e8bf5982490ee73
Patrick Niyogitare <54368821+PatrickNiyogitare28@users.noreply.github.com>
2021-06-09 09:09:20 +0200

    Update HibernateUtil.java

--- src/main/java/com/example/students_record/utils/HibernateUtil.java
+++ src/main/java/com/example/students_record/utils/HibernateUtil.java
@@ -23,8 +23,8 @@ public class HibernateUtil {
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
                 settings.put(Environment.URL, "jdbc:mysql://localhost:3306/students?useSSL=false");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "@vernom28_niyo");
+                settings.put(Environment.USER, "*****");
+                settings.put(Environment.PASS, "*****");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
 
                 settings.put(Environment.SHOW_SQL, "true");
